a42e286e3d7aaf1e0c55c8cbdc9baedd2710c7ac
leo <LevchenkoLM23@st.ithub.ru>
2025-01-21 10:52:24 +0300

    Prishli k pervoi pare

--- pkg/rpn/rpn.go
+++ pkg/rpn/rpn.go
@@ -1,14 +1,29 @@
 package rpn
 
 import (
-    "fmt"
-    // "net/http"
-    // "sync"
-    // "time"
-    // "strconv"
+    "net/http"
+    "os"
+    "html/template"
 )
 
-func PrintTest() {
-    fmt.Println("test")
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    tpl.Execute(w, nil)
 }
 
+func Main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3000"
+    }
+
+    mux := http.NewServeMux()
+
+    // Добавьте следующие две строки
+    fs := http.FileServer(http.Dir("assets"))
+    mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+    mux.HandleFunc("/", indexHandler)
+    http.ListenAndServe(":"+port, mux)
+}
